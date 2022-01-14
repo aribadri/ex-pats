0a741d87539d52040b547dbabc50f4d88a55347c
aribadri <91500048+aribadri@users.noreply.github.com>
2022-01-14 14:31:09 +0300

    google map added

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,15 +1,19 @@
+// import '../../App.css';
 import { Outlet, Link } from 'react-router-dom';
 import MyButton from '../../UI/button/MyButton';
+
 // import Loader from '../UI/loader/Loader';
 
 // eslint-disable-next-line react/prop-types
-function Layout({ setModal }) {
+function Layout({ setModal, userCity }) {
   return (
     <>
       <header className="App-header">
         <Link to="/">Xpat</Link>
-        <MyButton type="button" className="btn btn-primary" onClick={() => setModal(true)}>Login/Registration</MyButton>
+        <MyButton type="button" className="btn btn-primary" onClick={() => setModal(true)}>Войти</MyButton>
         {/* <Loader /> */}
+        Вы находитесь в |
+        {userCity}
       </header>
       <Outlet />
     </>
