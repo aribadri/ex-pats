ccca25170e11b6ab58a20e5b0d08ac4b3bf62b94
aribadri <91500048+aribadri@users.noreply.github.com>
2022-01-20 14:46:45 +0300

    chatformlibrary

---
+++ client/src/components/ChatLibrary/ChatLibrary.jsx
@@ -0,0 +1,72 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-return-assign */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Talk from 'talkjs';
+
+class ChatLibrary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.inbox = undefined;
+  }
+
+  componentDidMount() {
+    // Promise can be `then`ed multiple times
+    Talk.ready
+      .then(() => {
+        const me = new Talk.User({
+          id: this.props.userChat.id,
+          name: `${this.props.userChat.first_name}`,
+          email: 'george@looney.net',
+          photoUrl: `http://localhost:5000${this.props.userChat.avatar_link}`,
+          welcomeMessage: 'Hey there! How are you? :-)',
+        });
+
+        if (!window.talkSession) {
+          window.talkSession = new Talk.Session({
+            appId: 'tXXcUfCQ',
+            me,
+          });
+        }
+
+        const other = new Talk.User({
+          id: this.props.userChat.id,
+          name: `${this.props.userChat.first_name}`,
+          email: 'george@looney.net',
+          photoUrl: `http://localhost:5000${this.props.userChat.avatar_link}`,
+          welcomeMessage: 'Hey there! Love to chat :-)',
+        });
+
+        // You control the ID of a conversation. oneOnOneId is a helper method that generates
+        // a unique conversation ID for a given pair of users.
+        const conversationId = Talk.oneOnOneId(me, other);
+
+        const conversation = window.talkSession.getOrCreateConversation(conversationId);
+        conversation.setParticipant(me);
+        conversation.setParticipant(other);
+
+        this.inbox = window.talkSession.createInbox({
+          selected: conversation,
+        });
+        this.inbox.mount(this.container);
+      })
+      .catch((e) => console.error(e));
+  }
+
+  componentWillUnmount() {
+    if (this.inbox) {
+      this.inbox.destroy();
+    }
+  }
+
+  render() {
+    return (
+      <span>
+        <div style={{ height: '500px' }} ref={(c) => this.container = c}>Loading...</div>
+      </span>
+    );
+  }
+}
+
+export default ChatLibrary;
